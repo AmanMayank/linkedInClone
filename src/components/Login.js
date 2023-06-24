694f4031f50da068542b7b34bfbf30f83fa1eb95
AmanMayank <aman.msingh87@gmail.com>
2023-06-24 17:31:36 +0530

    Login Page UI Complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,6 +13,20 @@ function Login() {
           <SignIn>Sign In</SignIn>
         </div>
       </Nav>
+
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="login" />
+        </Hero>
+
+        <Form>
+          <Google>
+            <img src="/images/google.svg" alt="" />
+            Sign in with Google
+          </Google>
+        </Form>
+      </Section>
     </Container>
   );
 }
@@ -74,6 +88,88 @@ const SignIn = styled.a`
     color: #0a66c2;
     text-decoration: none;
     cursor: pointer;
+  }
+`;
+
+const Section = styled.section`
+  align-content: start;
+  display: flex;
+  min-height: 700px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+
+const Form = styled.div`
+  z-index: 25;
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0/60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0, 0, 0, /0);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgbs(0, 0, 0, 0.6);
+
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    cursor: pointer;
+    color: rgba(0, 0, 0, 0.75);
   }
 `;
 
