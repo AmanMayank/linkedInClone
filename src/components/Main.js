89f93c60cb5c3b319cb9554a1f5fd6e06faee80a
AmanMayank <aman.msingh87@gmail.com>
2023-07-14 20:20:02 +0530

    article template

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { AiFillFileImage, AiTwotoneVideoCamera } from "react-icons/ai";
+import { BsCalendarEvent, BsThreeDots } from "react-icons/bs";
+import { GrArticle } from "react-icons/gr";
 
 function Main() {
   return (
@@ -11,26 +14,64 @@ function Main() {
         </div>
         <div>
           <button>
-            <img src="/images/photo-icon.svg" alt="" />
+            {/* <img src="/images/photo-icon.svg" alt="" /> */}
+            <div>
+              <AiFillFileImage></AiFillFileImage>
+            </div>
             <span>Photo</span>
           </button>
 
           <button>
-            <img src="/images/video-icon.svg" alt="" />
+            {/* <img src="/images/video-icon.svg" alt="" /> */}
+            <div>
+              <AiTwotoneVideoCamera></AiTwotoneVideoCamera>
+            </div>
             <span>Video</span>
           </button>
 
           <button>
-            <img src="/images/event-icon.svg" alt="" />
+            {/* <img src="/images/event-icon.svg" alt="" /> */}
+            <div>
+              <BsCalendarEvent></BsCalendarEvent>
+            </div>
             <span>Event</span>
           </button>
 
           <button>
-            <img src="/images/article-icon.svg" alt="" />
+            {/* <img src="/images/article-icon.svg" alt="" /> */}
+            <div>
+              <GrArticle></GrArticle>
+            </div>
             <span>Write article</span>
           </button>
         </div>
       </ShareBox>
+
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+
+            <button>
+              <BsThreeDots></BsThreeDots>
+            </button>
+          </SharedActor>
+
+          <Description>Description</Description>
+          <Sharedimg>
+            <a>
+              <img src="/images/green-1548945.jpg" alt="" />
+            </a>
+          </Sharedimg>
+        </Article>
+      </div>
     </Container>
   );
 }
@@ -91,6 +132,7 @@ const ShareBox = styled(CommonCard)`
         text-align: left;
       }
     }
+
     &:nth-child(2) {
       display: flex;
       flex-wrap: wrap;
@@ -98,14 +140,98 @@ const ShareBox = styled(CommonCard)`
       padding-bottom: 4px;
 
       button {
-        img {
-          margin: 0 4px 0 -2px;
+        div {
+          // margin: 0 4px 0 -2px;
+          line-height: 1.5;
+          align-items: center;
+          margin-bottom: 12px;
+          margin-right: -11px;
         }
         span {
           color: #70b5f9;
         }
       }
     }
+  }
+`;
+
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flx-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 5px;
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 16px;
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+const Sharedimg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
   }
 `;
 
